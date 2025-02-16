77bc1f6529de6818eed3cb78ac0b8098c1cbf431
Yoshi Odei <yoshiodei@yahoo.com>
2025-02-16 09:00:42 +0000

    error fix 001

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
     images: {
         remotePatterns: [
           {
@@ -10,7 +9,17 @@ const nextConfig = {
           },
         ],
       },
-      
+      reactStrictMode: true,
+      pageExtensions: [
+        "page.tsx",
+        "page.ts",
+        // FIXME: Next.js has a bug which does not resolve not-found.page.tsx corretly
+        // Instead, use `not-found.ts` as a workaround
+        // "ts" is required to resolve `not-found.ts`
+        // https://github.com/vercel/next.js/issues/65447
+        "ts"
+    
+      ],
 };
 
 export default nextConfig;
