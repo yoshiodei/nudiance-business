1988f765aa4c7e3697bc27b86353f18204581277
Yoshi Odei <yoshiodei@yahoo.com>
2025-02-16 09:51:34 +0000

    modify not found page

--- src/app/_not-found-component.tsx
+++ src/app/_not-found-component.tsx
@@ -2,10 +2,8 @@
 import Error from "next/error";
 export default function NotFound() {
   return (
-    <html>
-    <body>
+    <>
     <Error statusCode={404}/>
-    </body>
-    </html>
+    </>
   );
 }
